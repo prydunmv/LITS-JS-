748c89f74df77b0af7d58495abf2c06edc607086
mykola <prydumv@gmail.com>
2020-03-31 21:35:19 +0300

    homework_balans

---
+++ js/cash.js
@@ -0,0 +1,25 @@
+const ncome = [ 200, 345, 234, 123, 456, 234, 398,768 ];
+const costs = [23, 23, 45, 56, 234, 233, 134, 234, 33 ];
+
+let sumNcome = 0;
+let sumCosts = 0;
+
+for (let i = 0; i < ncome.length; i++) {
+    sumNcome = sumNcome + (ncome[i]);
+}
+
+for (let i = 0; i < costs.length; i++) {
+    sumCosts = sumCosts + (costs[i]);
+}
+
+let balans = sumNcome - sumCosts;
+
+console.log(sumNcome);
+console.log(sumCosts);
+console.log(balans);
+
+if (balans > 0){
+    console.log("Вітаю ваш баланс плюсовий і дорівноє- "+balans);
+} else { 
+      console.log( "Нажаль ваш баланс нижче нуля і дорівноє- "+balans);
+  }
