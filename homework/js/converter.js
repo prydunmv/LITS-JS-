13de6c0f0ff2fdd75899bc33ff41898f912d8ccd
mykola <prydumv@gmail.com>
2020-04-22 15:19:57 +0300

    include course

--- homework/js/converter.js
+++ homework/js/converter.js
@@ -1,16 +1,32 @@
 
-const valutes = {
-    'долар': 27,
+let valutes = {
+    'долар': 25,
     'євро': 30,
     'рубль': 0.20,
     'злоті': 7,
     'фунт стерлінг': 32
 }
 
+async function getCurrency() {
+ const response = await fetch('https://api.exchangeratesapi.io/latest');
+ let valut = await response.json();
+let course = valut.rates;
+    valutes['долар'] = course.USD
+    valutes['канадський долар'] = course.CAD,
+    valutes['рубль'] = course.RUB,
+    valutes['злоті'] = course.PLN,
+    valutes['фунт стерлінг'] =  course.AUD
+}
+
+
 function myFunction() {
     let valute = $('#valutes')[0].value;
     let course = valutes[valute];
+    console.log(valutes);
     let number = 0;
     number = document.getElementById('number').value;
     document.getElementById("text").innerHTML = "за "+ number + " одиниць ви отримаєтете в гривневому еквіваленті " +course * number +" грн";
   }
+
+getCurrency()
+
